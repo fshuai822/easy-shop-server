d415ca556008c13321cf81963a593ceeb40a1757
shuaifeng <fshuai@umich.edu>
2022-06-18 20:48:06 -0400

    first commit

--- app.js
+++ app.js
@@ -54,8 +54,12 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 })
 
 //server
-app.listen(3000,() =>{
-    
-    console.log('server is running http://locationhost:3000')
-})
+// app.listen(3000,() =>{
+//
+//     console.log('server is running http://locationhost:3000')
+// })
 
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port" + port)
+})
